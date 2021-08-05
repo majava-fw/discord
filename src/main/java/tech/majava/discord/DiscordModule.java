20063da575fb6ceb1d6d5305a067627121e0b169
majksa <info@majksa.cz>
2021-08-06 00:53:17 +0200

    JDA setup core + templating

---
+++ src/main/java/tech/majava/discord/DiscordModule.java
@@ -0,0 +1,83 @@
+/*
+ *  discord - tech.majava.discord.DiscordModule
+ *  Copyright (C) 2021  Majksa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package tech.majava.discord;
+
+import cz.majksa.commons.majava.context.ApplicationContext;
+import cz.majksa.commons.majava.listeners.ListenersModule;
+import cz.majksa.commons.majava.logging.LoggingModule;
+import cz.majksa.commons.majava.modules.Module;
+import lombok.Getter;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.events.GenericEvent;
+import tech.majava.discord.config.DiscordConfig;
+import tech.majava.discord.listeners.DiscordEventsHandler;
+
+import javax.annotation.Nonnull;
+import javax.security.auth.login.LoginException;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * <p><b>Class {@link tech.majava.discord.DiscordModule}</b></p>
+ *
+ * @author majksa
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Getter
+public final class DiscordModule extends Module<DiscordConfig> {
+
+    private ListenersModule listeners;
+    private LoggingModule logging;
+    private JDA jda;
+    private DiscordEventsHandler handler;
+
+    public DiscordModule(@Nonnull DiscordConfig config, @Nonnull ApplicationContext context) {
+        super(config, context, "discord", "discord module for creating bots in majava");
+        dependencies.add(ListenersModule.class);
+        dependencies.add(LoggingModule.class);
+    }
+
+    @Nonnull
+    @Override
+    protected CompletableFuture<Void> onStart() {
+        return CompletableFuture.runAsync(() -> {
+            listeners = context.getModules().get(ListenersModule.class);
+            logging = context.getModules().get(LoggingModule.class);
+            try {
+                jda = config.getJda().toBuilder().build().awaitReady();
+                handler = new DiscordEventsHandler(logging, jda);
+                listeners.registerHandler(GenericEvent.class, handler);
+            } catch (InterruptedException | LoginException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    @Nonnull
+    @Override
+    protected CompletableFuture<Void> onShutdown() {
+        return CompletableFuture.runAsync(() -> {
+            handler.stop();
+            listeners.unregisterHandler(GenericEvent.class);
+            jda.shutdown();
+        });
+    }
+
+}
---
+++ src/main/java/tech/majava/discord/templating/MessageTemplateImpl.java
@@ -0,0 +1,79 @@
+/*
+ *  majbot - cz.majksa.majbot.templating.MessageTemplateImpl
+ *  Copyright (C) 2021  Majksa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package tech.majava.discord.templating;
+
+import lombok.Getter;
+import lombok.NonNull;
+import net.dv8tion.jda.api.EmbedBuilder;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * <p><b>Class {@link tech.majava.discord.templating.MessageTemplateImpl}</b></p>
+ *
+ * @author majksa
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Getter
+public class MessageTemplateImpl implements MessageTemplate {
+
+    private final @NonNull List<EmbedBuilder> embedBuilders;
+    private final @NonNull EmbedBuilder defaultEmbedBuilder;
+    private final @NonNull StringBuilder contentBuilder;
+
+    /**
+     * A constructor
+     */
+    public MessageTemplateImpl() {
+        this(new EmbedBuilder());
+    }
+
+    /**
+     *
+     * @param defaultEmbedBuilder {@link #defaultEmbedBuilder}
+     */
+    public MessageTemplateImpl(@NonNull EmbedBuilder defaultEmbedBuilder) {
+        this(new LinkedList<>(), defaultEmbedBuilder, new StringBuilder());
+    }
+
+    /**
+     * A constructor
+     *
+     * @param template the template to copy
+     */
+    public MessageTemplateImpl(@NonNull MessageTemplate template) {
+        this(template.getEmbedBuilders(), template.getDefaultEmbedBuilder(), template.getContentBuilder());
+    }
+
+    /**
+     * A constructor
+     *
+     * @param embedBuilders       {@link #embedBuilders}
+     * @param defaultEmbedBuilder {@link #defaultEmbedBuilder}
+     * @param contentBuilder      {@link #contentBuilder}
+     */
+    public MessageTemplateImpl(@NonNull List<EmbedBuilder> embedBuilders, @NonNull EmbedBuilder defaultEmbedBuilder, @NonNull StringBuilder contentBuilder) {
+        this.embedBuilders = embedBuilders;
+        this.defaultEmbedBuilder = defaultEmbedBuilder;
+        this.contentBuilder = contentBuilder;
+    }
+
+}
